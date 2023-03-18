ee95db348177de4a34a57240212d8ef9ac80c582
fahmiaga <fahmi.aga@gmail.com>
2023-03-18 09:38:33 +0700

    Add project cards and hover

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LeftArrow from "../assets/image/arrow-right-short.svg";
 
 const Project = () => {
   return (
@@ -8,17 +9,75 @@ const Project = () => {
           <h1 className="text-4xl text-white text-center">Project</h1>
           <hr className="w-full border-yellow-600 border-t-4 ml-2" />
         </div>
-        <div className="flex mt-16">
-          <p className="text-yellow-600">
-            "Building websites is like building a puzzle. Every piece has to fit
-            perfectly to create a beautiful picture."
-          </p>
-          <p className="pl-56">
-            An enthusiastic Web Developer with exceptional flexibility as well
-            as a keen desire to improve and develop new skills.good
-            communication, and good attention to detail and a high level of
-            accuracy regarding quality and presentation of work.
-          </p>
+        <div className="w-full grid grid-cols-2 gap-4 mt-16">
+          <div className="w-full bg-slate-200 h-64 relative group transition-transform duration-500">
+            <div className="card-hovered"></div>
+            <div className="hovered-content h-full flex flex-col justify-center items-center">
+              <h3 className="text-black text-4xl font-semibold">
+                Portfolio Item
+              </h3>
+              <h3 className="py-4 text-2xl">Mangalime</h3>
+              <div className="w-14 h-14 bg-transparent border-solid border-black border-2 rounded-full flex justify-center items-center ease-out duration-300 transition-all cursor-pointer hover:bg-white">
+                <img src={LeftArrow} alt="special" className="w-10" />
+              </div>
+            </div>
+            <img
+              src="https://plus.unsplash.com/premium_photo-1671269943771-63db2ab54bf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="tet"
+              className="w-full h-64 object-cover"
+            />
+          </div>
+          <div className="w-full bg-slate-200 h-64 relative group transition-transform duration-500">
+            <div className="card-hovered"></div>
+            <div className="hovered-content h-full flex flex-col justify-center items-center">
+              <h3 className="text-black text-4xl font-semibold">
+                Portfolio Item
+              </h3>
+              <h3 className="py-4 text-2xl">Mangalime</h3>
+              <div className="w-14 h-14 bg-transparent border-solid border-black border-2 rounded-full flex justify-center items-center ease-out duration-300 transition-all cursor-pointer hover:bg-white">
+                <img src={LeftArrow} alt="special" className="w-10" />
+              </div>
+            </div>
+            <img
+              src="https://plus.unsplash.com/premium_photo-1671269943771-63db2ab54bf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="tet"
+              className="w-full h-64 object-cover"
+            />
+          </div>
+          <div className="w-full bg-slate-200 h-64 relative group transition-transform duration-500">
+            <div className="card-hovered"></div>
+            <div className="hovered-content h-full flex flex-col justify-center items-center">
+              <h3 className="text-black text-4xl font-semibold">
+                Portfolio Item
+              </h3>
+              <h3 className="py-4 text-2xl">Mangalime</h3>
+              <div className="w-14 h-14 bg-transparent border-solid border-black border-2 rounded-full flex justify-center items-center ease-out duration-300 transition-all cursor-pointer hover:bg-white">
+                <img src={LeftArrow} alt="special" className="w-10" />
+              </div>
+            </div>
+            <img
+              src="https://plus.unsplash.com/premium_photo-1671269943771-63db2ab54bf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="tet"
+              className="w-full h-64 object-cover"
+            />
+          </div>
+          <div className="w-full bg-slate-200 h-64 relative group transition-transform duration-500">
+            <div className="card-hovered"></div>
+            <div className="hovered-content h-full flex flex-col justify-center items-center">
+              <h3 className="text-black text-4xl font-semibold">
+                Portfolio Item
+              </h3>
+              <h3 className="py-4 text-2xl">Mangalime</h3>
+              <div className="w-14 h-14 bg-transparent border-solid border-black border-2 rounded-full flex justify-center items-center ease-out duration-300 transition-all cursor-pointer hover:bg-white">
+                <img src={LeftArrow} alt="special" className="w-10" />
+              </div>
+            </div>
+            <img
+              src="https://plus.unsplash.com/premium_photo-1671269943771-63db2ab54bf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="tet"
+              className="w-full h-64 object-cover"
+            />
+          </div>
         </div>
       </div>
     </>
